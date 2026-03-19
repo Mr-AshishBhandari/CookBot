40cf55393f69ff4e40ba6403c247c6c6ea31c8bb
Ashish <ashishbhandari675@gmail.com>
2026-03-19 13:24:33 +0545

    reciepe->phase 1

---
+++ main.py
@@ -0,0 +1,46 @@
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from langchain.messages import AIMessage , SystemMessage , HumanMessage
+
+import streamlit as st
+
+import os
+import dotenv
+dotenv.load_dotenv()
+
+HUGGINGFACEHUB_ACCESS_TOKEN = os.getenv('HUGGINGFACEHUB_ACCESS_TOKEN')
+llm = HuggingFaceEndpoint(repo_id='meta-llama/Llama-3.1-8B-Instruct',huggingfacehub_api_token=HUGGINGFACEHUB_ACCESS_TOKEN)
+model = ChatHuggingFace(llm=llm)
+
+system_message = SystemMessage(content='''
+                               You are an expert chef and recipe generator. Your task is to create complete, clear, and easy-to-follow recipes based on the ingredients provided by the user. Follow these rules:
+
+1. Start with a **recipe title**.
+2. List all **ingredients with quantities**.
+3. Provide **step-by-step instructions** for preparing the dish.
+4. Keep instructions simple, concise, and easy to follow.
+5. Suggest optional **tips or variations** if appropriate.
+6. Assume the cuisine can be adapted to the user's region or style (e.g., Nepali, vegetarian, quick meals) if not specified.
+7. Avoid adding ingredients that are not listed unless necessary for basic cooking (like salt, water, or oil).
+
+Respond in **clear, structured text** that a user can immediately follow to cook the dish.
+                               Reply if the use provide positive or negaitve feedback.
+                               '''
+                               )
+history = []
+
+if "history" not in st.session_state:
+    st.session_state.history = [system_message]
+
+
+prompt = st.chat_input("Your message ...")
+
+if prompt:
+    st.session_state.history.append(HumanMessage(content=prompt))
+
+    result = model.invoke(st.session_state.history)
+
+    st.session_state.history.append(AIMessage(content=result.content))
+
+    st.write(result.content)
+
+    # st.write(st.session_state)
